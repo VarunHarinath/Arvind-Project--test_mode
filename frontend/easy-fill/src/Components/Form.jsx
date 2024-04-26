817b5aa38b02c972b3e3d104e022816375eae1da
Arvind <2211CS010458@mallareddyuniversity.ac.in>
2024-04-26 14:52:53 +0530

    update on city bug

--- frontend/easy-fill/src/Components/Form.jsx
+++ frontend/easy-fill/src/Components/Form.jsx
@@ -211,25 +211,27 @@ export default function Form() {
                   </div>
 
                   <div className="sm:col-span-3">
-                    <label
-                      htmlFor="country"
-                      className="block text-sm font-medium leading-6 text-gray-900"
-                    >
-                      City
-                    </label>
-                    <div className="mt-2">
-                      <select
-                        id="country"
-                        name="country"
-                        autoComplete="country-name"
-                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
-                      >
-                        <option>United States</option>
-                        <option>Canada</option>
-                        <option>Mexico</option>
-                      </select>
-                    </div>
-                  </div>
+      <label
+        htmlFor="country"
+        className="block text-sm font-medium leading-6 text-gray-900"
+      >
+        City
+      </label>
+      <div className="mt-2">
+        <select
+          id="country"
+          name="country"
+          autoComplete="country-name"
+          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+          value={city} // Set the value of the select to the state variable
+          onChange={()=>setCity(event.target.value)} // Call the handler when the select value changes
+        >
+          <option value="Hyderabad">Hyderabad</option>
+          <option value="Bangalore">Bangalore</option>
+          <option value="Delhi">Delhi</option>
+        </select>
+      </div>
+    </div>
 
                   <div className="col-span-full">
                     <label
