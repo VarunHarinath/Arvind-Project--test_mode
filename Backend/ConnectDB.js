613ca435027d552337a16f82f6334031afeb6e8c
2211cs010458 <157341384+2211cs010458@users.noreply.github.com>
2024-06-01 18:09:11 +0530

    final update

--- Backend/ConnectDB.js
+++ Backend/ConnectDB.js
@@ -3,10 +3,11 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     await mongoose.connect("mongodb://localhost:27017/FuelX");
-    console.log("connectedo mongodb");
+    console.log("connected to mongodb");
   } catch (error) {
     console.log(error);
   }
 };
 
 export default connectDB;
+ 
